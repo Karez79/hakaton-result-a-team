286952b749342428ff2dc4a8de80064b6122fe42
Иван Козлов <karez78@yandex.ru>
2025-03-07 22:28:03 +0300

    add background

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,20 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module';
 
 export class BackgroundModule extends Module {
+  constructor() {
+    super('background', 'Поменять фон');
+  }
 
+  trigger() {
+    document.body.style.backgroundColor = this.getRandomColor();
+  }
+
+  getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 }
