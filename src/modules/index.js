b34ba17f3edce71d72a4bb1f2e122172c999c767
Иван Козлов <karez78@yandex.ru>
2025-03-09 02:45:58 +0300

    Updates

--- src/modules/index.js
+++
@@ -1,6 +0,0 @@
-import { BackgroundModule } from "./background.module";
-import { ClicksModule } from "./clicks.module";
-import { MessageModule } from "./message.modules";
-import { TimerModule } from "./timer.module";
-
-export const modules = [new BackgroundModule(),new ClicksModule(), new MessageModule(),new TimerModule()]
