7ff1c699f67c3c8cea27a1dca589b6b941811700
Egor Madakalov <gpt.e.madakalov@gmail.com>
2025-03-09 03:43:22 +0500

    create reexport

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import './styles.css'
 import { useFetch } from './api'
 import { random } from './utils'
 import { MessageModule } from './modules/message.modules'
+import { modules } from './modules'
 
 useFetch({url: `https://jsonplaceholder.typicode.com/posts/${random(1, 100)}`})
 
@@ -9,7 +10,8 @@ useFetch({url: `https://jsonplaceholder.typicode.com/posts/${random(1, 100)}`})
 const message  = new MessageModule();
 message.renderMessage()
 
-
+console.log(message)
 document.querySelector("#message")?.addEventListener('click', () => {
     message.trigger()
 })
+
